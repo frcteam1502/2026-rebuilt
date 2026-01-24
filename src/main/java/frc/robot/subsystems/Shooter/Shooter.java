807a88fb93363c790d60f090c1f66cd012529d87
lesserc <cdlesser@provide.net>
2026-01-24 11:22:52 -0500

    Start shooter config

--- src/main/java/frc/robot/subsystems/Shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter/Shooter.java
@@ -4,6 +4,11 @@
 
 package frc.robot.subsystems.Shooter;
 
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.revrobotics.PersistMode;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel;
 import com.revrobotics.spark.SparkMax;
@@ -12,6 +17,7 @@ import com.revrobotics.spark.config.EncoderConfig;
 import com.revrobotics.spark.config.FeedForwardConfig;
 import com.revrobotics.spark.config.SparkFlexConfig;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -24,31 +30,54 @@ public class Shooter extends SubsystemBase {
   private final SparkFlex indexerMotor = ShooterCfg.INDEXER_MOTOR;
   private final SparkMax turretMotor = ShooterCfg.TURRET_MOTOR;
 
+  //RelativeEncoder for all motors
+  RelativeEncoder shooterLeadEncoder;
+  //TODO - Create RelativeEncoder for all motors
+
+  //CANCoder objects for turret and hood encoders
+  private final CANcoder turretAbsEncoder = ShooterCfg.TURRET_ABS_ENCODER;
+  //TODO - Create CANcoder for hood absolute encoder
+
+  //REV PIDF control objects
+  SparkClosedLoopController shooterPIDController;
+
+  //WPI PID control objects
+  private final PIDController turretPIDController = new PIDController(ShooterCfg.TURRET_P_GAIN,
+                                                                      ShooterCfg.TURRET_I_GAIN,
+                                                                      ShooterCfg.TURRET_D_GAIN);
+
+  //TODO - Creat WPI PID for hood
+
+  //Shooter lead motor config - REV closed loop speed control
+  private final SparkFlexConfig shooterLeadConfig = new SparkFlexConfig();
   private final EncoderConfig shooterLeadEncoderConfig = new EncoderConfig();
   private final ClosedLoopConfig shooterLeadPIDFConfig = new ClosedLoopConfig();
   private final FeedForwardConfig shooterLeadFFConfig = new FeedForwardConfig();
-  private final SparkFlexConfig shooterLeadConfig = new SparkFlexConfig();
 
-  private final EncoderConfig shooterFollowerEncoderConfig = new EncoderConfig();
+  //Shooter follower motor config - follow lead motor
   private final SparkFlexConfig shooterFollowerConfig = new SparkFlexConfig();
+  private final EncoderConfig shooterFollowerEncoderConfig = new EncoderConfig();
 
-  private final EncoderConfig shooterHoodEncoderConfig = new EncoderConfig();
-  private final ClosedLoopConfig shooterHoodPIDFConfig = new ClosedLoopConfig();
-  private final FeedForwardConfig shooterHoodFFConfig = new FeedForwardConfig();
+  //Hood motor config - WPI position control 
   private final SparkFlexConfig shooterHoodConfig = new SparkFlexConfig();
-
+  private final FeedForwardConfig shooterHoodFFConfig = new FeedForwardConfig();
+  
+  //Feed motor - open loop
   private final EncoderConfig shooterFeedEncoderConfig = new EncoderConfig();
   private final SparkFlexConfig shooterFeedConfig = new SparkFlexConfig();
 
+  //Index motor - open loop
   private final EncoderConfig shooterIndexerEncoderConfig = new EncoderConfig();
   private final SparkFlexConfig shooterIndexerConfig = new SparkFlexConfig();
 
-  private final EncoderConfig shooterTurretEncoderConfig = new EncoderConfig();
-  private final ClosedLoopConfig shooterTurretPIDFConfig = new ClosedLoopConfig();
-  private final FeedForwardConfig shooterTurretFFConfig = new FeedForwardConfig();
+  //Turret motor - WPI position control
   private final SparkFlexConfig shooterTurretConfig = new SparkFlexConfig();
+  private final FeedForwardConfig shooterTurretFFConfig = new FeedForwardConfig();
+  
 
-  public Shooter() {}
+  public Shooter() {
+    configShooterMotors();
+  }
 
   @Override
   public void periodic() {
@@ -56,6 +85,50 @@ public class Shooter extends SubsystemBase {
   
   }
   private void configShooterMotors() {
+    //Config the encoders
+    shooterLeadEncoder = leadShooterMotor.getEncoder();
+    shooterLeadEncoderConfig.positionConversionFactor(ShooterCfg.SHOOTER_ENC_POS_CONFIG);
+    shooterLeadEncoderConfig.velocityConversionFactor(ShooterCfg.SHOOTER_ENC_VEL_CONFIG);
 
+    //Config PID values
+    shooterPIDController = leadShooterMotor.getClosedLoopController();
+    shooterLeadPIDFConfig.p(ShooterCfg.SHOOTER_P_GAIN);
+    shooterLeadPIDFConfig.i(ShooterCfg.SHOOTER_I_GAIN);
+    shooterLeadPIDFConfig.d(ShooterCfg.SHOOTER_D_GAIN);
+
+    //Config FF values
+    shooterLeadFFConfig.kV(ShooterCfg.SHOOTER_KV);
+    shooterLeadFFConfig.kA(ShooterCfg.SHOOTER_KA);
+    shooterLeadFFConfig.kS(ShooterCfg.SHOOTER_KS);
+    
+    shooterLeadPIDFConfig.apply(shooterLeadFFConfig);
+
+    //Config SparkFlex
+    shooterLeadConfig.inverted(ShooterCfg.SHOOTER_LEAD_INVERTED);
+    shooterLeadConfig.idleMode(ShooterCfg.SHOOTER_IDLE_MODE);
+    shooterLeadConfig.smartCurrentLimit(ShooterCfg.SHOOTER_CURRENT_LIMIT);
+
+    //Apply Encoder and Closed Loop Configs to the SparkFlexCfg
+    shooterLeadConfig.apply(shooterLeadEncoderConfig);
+    shooterLeadConfig.apply(shooterLeadPIDFConfig);
+
+    //Write to the SparkFlex
+    leadShooterMotor.configure(shooterLeadConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    //Setup the follower motor
+    //Config the encoders
+    shooterFollowerEncoderConfig.positionConversionFactor(ShooterCfg.SHOOTER_ENC_POS_CONFIG);
+    shooterFollowerEncoderConfig.velocityConversionFactor(ShooterCfg.SHOOTER_ENC_VEL_CONFIG);
+
+    //Config Spark Flex
+    shooterFollowerConfig.follow(ShooterCfg.LEAD_SHOOTER_MOTOR_ID);
+    shooterFollowerConfig.inverted(ShooterCfg.SHOOTER_FOLLOW_INVERTED);
+    shooterFollowerConfig.idleMode(ShooterCfg.SHOOTER_IDLE_MODE);
+    shooterFollowerConfig.smartCurrentLimit(ShooterCfg.SHOOTER_CURRENT_LIMIT);
+
+    shooterFollowerConfig.apply(shooterLeadEncoderConfig);
+
+    //Write to the SparkFlex
+    followerShooterMotor.configure(shooterFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
 }
--- src/main/java/frc/robot/subsystems/Shooter/ShooterCfg.java
+++ src/main/java/frc/robot/subsystems/Shooter/ShooterCfg.java
@@ -1,8 +1,10 @@
 package frc.robot.subsystems.Shooter;
 
+import com.ctre.phoenix6.hardware.CANcoder;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 public class ShooterCfg {
     //Shooter Motor Config
@@ -13,12 +15,38 @@ public class ShooterCfg {
     public static final int INDEXER_MOTOR_ID = 12;
     public static final int TURRET_MOTOR_ID = 13;
 
+    //CAN encoder IDs
+    public static final int TURRET_ABS_ENCODER_ID = 13;
+
     public static final SparkFlex LEAD_SHOOTER_MOTOR = new SparkFlex(LEAD_SHOOTER_MOTOR_ID, SparkLowLevel.MotorType.kBrushless);
     public static final SparkFlex FOLLOWER_SHOOTER_MOTOR = new SparkFlex(FOLLOWER_SHOOTER_MOTOR_ID, SparkLowLevel.MotorType.kBrushless);
     public static final SparkMax HOOD_MOTOR = new SparkMax(HOOD_MOTOR_ID, SparkLowLevel.MotorType.kBrushless);
     public static final SparkFlex FEED_MOTOR = new SparkFlex(FEED_MOTOR_ID, SparkLowLevel.MotorType.kBrushless);
     public static final SparkFlex INDEXER_MOTOR = new SparkFlex(INDEXER_MOTOR_ID, SparkLowLevel.MotorType.kBrushless);
     public static final SparkMax TURRET_MOTOR = new SparkMax(TURRET_MOTOR_ID, SparkLowLevel.MotorType.kBrushless);
+    public static final CANcoder TURRET_ABS_ENCODER = new CANcoder(TURRET_ABS_ENCODER_ID);
+    
+    //PIDF Gains
+    public static final double SHOOTER_P_GAIN = 0;
+    public static final double SHOOTER_I_GAIN = 0;
+    public static final double SHOOTER_D_GAIN = 0;
+
+    public static final double TURRET_P_GAIN = 0;
+    public static final double TURRET_I_GAIN = 0;
+    public static final double TURRET_D_GAIN = 0;
+
+
+    public static final double SHOOTER_ENC_POS_CONFIG = 1;
+    public static final double SHOOTER_ENC_VEL_CONFIG = 1;
+    public static final double SHOOTER_KV = 0;
+    public static final double SHOOTER_KA = 0;
+    public static final double SHOOTER_KS = 0;
+
+    public static final boolean SHOOTER_LEAD_INVERTED = false;
+    public static final IdleMode SHOOTER_IDLE_MODE = IdleMode.kCoast;
+    public static final int SHOOTER_CURRENT_LIMIT = 40;
+    public static final boolean SHOOTER_FOLLOW_INVERTED = !SHOOTER_LEAD_INVERTED;
+    
 
     
 }
