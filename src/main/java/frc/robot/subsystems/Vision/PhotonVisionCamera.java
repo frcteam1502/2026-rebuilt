59e60b5633768f553c9b52aa2c1a8877b20b0ada
jonathan berent <jonathan.berent@dx-fx.com>
2026-04-04 14:04:26 -0400

    remove unused vision code

--- src/main/java/frc/robot/subsystems/Vision/PhotonVisionCamera.java
+++ src/main/java/frc/robot/subsystems/Vision/PhotonVisionCamera.java
@@ -24,50 +24,25 @@
 
 package frc.robot.subsystems.Vision;
 
-import edu.wpi.first.apriltag.AprilTagFieldLayout;
-import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.Matrix;
-import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.kinematics.Odometry;
-import edu.wpi.first.math.numbers.N1;
-import edu.wpi.first.math.numbers.N3;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Optional;
-import java.util.function.Supplier;
 
 import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.PhotonUtils;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
-import org.photonvision.simulation.PhotonCameraSim;
-import org.photonvision.simulation.SimCameraProperties;
-import org.photonvision.simulation.VisionSystemSim;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
 public class PhotonVisionCamera {
     private final PhotonCamera camera;
     private final PhotonPoseEstimator photonEstimator;
-    private Matrix<N3, N1> curStdDevs;
     private double lastTimestamp;
-    private int numNewTargets = 0;
-    private double resultAmbiguity = -1.0;
-    private double resultDistance = -1.0;
     private Optional<EstimatedRobotPose> estimatedGlobalPose;
-
-    private boolean isAnyTargetFound = false;
-    private boolean isAnyValidTargetFound = false;
-    private boolean isNewResult = false;
-
-
 
     public PhotonVisionCamera(String cameraName, Transform3d robotToCam) {
         camera = new PhotonCamera(cameraName);
@@ -94,24 +69,14 @@ public class PhotonVisionCamera {
     public Optional<EstimatedRobotPose> processCamera(Pose2d referencePose){
         //Clear Estimated Pose in case no valid pose is found
         estimatedGlobalPose = Optional.empty();
-        isAnyValidTargetFound = false;
-        isAnyTargetFound = false;
-        isNewResult = false;
-        numNewTargets = 0;
-        resultAmbiguity = -2.0;
-        resultDistance = -1.0;
 
         //Read all results from the camera.
         PhotonPipelineResult pipelineResult = camera.getLatestResult();
-
-        isAnyTargetFound = pipelineResult.hasTargets();
 
         //Pass in the reference pose for the robot
         photonEstimator.setReferencePose(referencePose);
         //Return if no new results were received
         if(pipelineResult.getTimestampSeconds() == lastTimestamp) return estimatedGlobalPose;
-
-        isNewResult = true;
 
         //Find the targets too inaccurate to be used. 
         LinkedList<PhotonTrackedTarget> toRemove = new LinkedList<PhotonTrackedTarget>();
@@ -131,8 +96,6 @@ public class PhotonVisionCamera {
         //Return if the list of targets is non-existant or invalid
         if(!pipelineResult.hasTargets()) return estimatedGlobalPose;
 
-        isAnyValidTargetFound = true;
-
         var calculatedPose = photonEstimator.update(pipelineResult);
         
         if(calculatedPose.isPresent()){
@@ -148,100 +111,5 @@ public class PhotonVisionCamera {
                }
         };
         return estimatedGlobalPose;
-    }
-    /**
-     * The latest estimated robot pose on the field from vision data. This may be empty. This should
-     * only be called once per loop.
-     *
-     * <p>Also includes updates for the standard deviations, which can (optionally) be retrieved with
-     * {@link getEstimationStdDevs}
-     *
-     * @return An {@link EstimatedRobotPose} with an estimated pose, estimate timestamp, and targets
-     *     used for estimation.
-     */
-    public Optional<EstimatedRobotPose> getEstimatedGlobalPose() {
-        Optional<EstimatedRobotPose> visionEst = Optional.empty();
-        for (var change : camera.getAllUnreadResults()) {
-            visionEst = photonEstimator.update(change);
-            updateEstimationStdDevs(visionEst, change.getTargets());
-        }
-        return visionEst;
-    }
-
-    public Optional<EstimatedRobotPose> getProcessedGlobalPose() {
-        return estimatedGlobalPose;
-    }
-
-    /**
-     * Calculates new standard deviations This algorithm is a heuristic that creates dynamic standard
-     * deviations based on number of tags, estimation strategy, and distance from the tags.
-     *
-     * @param estimatedPose The estimated pose to guess standard deviations for.
-     * @param targets All targets in this camera frame
-     */
-    private void updateEstimationStdDevs(
-            Optional<EstimatedRobotPose> estimatedPose, List<PhotonTrackedTarget> targets) {
-        if (estimatedPose.isEmpty()) {
-            // No pose input. Default to single-tag std devs
-            curStdDevs = PhotonCameraCfg.SINGLE_TAG_STD_DEV;
-
-        } else {
-            // Pose present. Start running Heuristic
-            var estStdDevs = PhotonCameraCfg.SINGLE_TAG_STD_DEV;
-            int numTags = 0;
-            double avgDist = 0;
-
-            // Precalculation - see how many tags we found, and calculate an average-distance metric
-            for (var tgt : targets) {
-                var tagPose = photonEstimator.getFieldTags().getTagPose(tgt.getFiducialId());
-                if (tagPose.isEmpty()) continue;
-                numTags++;
-                avgDist +=
-                        tagPose
-                                .get()
-                                .toPose2d()
-                                .getTranslation()
-                                .getDistance(estimatedPose.get().estimatedPose.toPose2d().getTranslation());
-            }
-
-            if (numTags <= 1) {
-                // One or less tags visible. Default to single-tag std devs
-                curStdDevs = PhotonCameraCfg.SINGLE_TAG_STD_DEV;
-            } else {
-                // More than one tag visible, run the full heuristic.
-                avgDist /= numTags;
-                // Decrease std devs if multiple targets are visible
-                estStdDevs = PhotonCameraCfg.MULTI_TAG_STD_DEV;
-                // Increase std devs based on (average) distance
-                if (avgDist > 4) {
-                    estStdDevs = VecBuilder.fill(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
-                }else{
-                    estStdDevs = estStdDevs.times(1 + (avgDist * avgDist / 30));
-                }
-                curStdDevs = estStdDevs;
-            }
-        }
-    }
-
-    /**
-     * Returns the latest standard deviations of the estimated pose from {@link
-     * #getEstimatedGlobalPose()}, for use with {@link
-     * edu.wpi.first.math.estimator.SwerveDrivePoseEstimator SwerveDrivePoseEstimator}. This should
-     * only be used when there are targets visible.
-     */
-    public Matrix<N3, N1> getEstimationStdDevs() {
-        return curStdDevs;
-    }
-
-    public boolean doesCameraHaveAnyTargets(){
-        return isAnyTargetFound;
-    }
-
-    public boolean doesCameraHaveAnyValidTargets(){
-        return isAnyValidTargetFound;
-    }
-
-    public boolean doesCameraHaveNewTargets(){
-        return isNewResult;
     }
 }
