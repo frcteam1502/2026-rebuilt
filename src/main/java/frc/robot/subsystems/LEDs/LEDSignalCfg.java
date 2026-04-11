816d01068bf92f11ecad1e327e6050e298d8d4b0
jonathan berent <jonathan.berent@dx-fx.com>
2026-04-11 12:43:20 -0400

    update LED PWM channel to 0

--- src/main/java/frc/robot/subsystems/LEDs/LEDSignalCfg.java
+++ src/main/java/frc/robot/subsystems/LEDs/LEDSignalCfg.java
@@ -3,18 +3,14 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems.LEDs;
-import com.revrobotics.spark.SparkFlex;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-
-import edu.wpi.first.wpilibj.PWM;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 
 /** Add your docs here. */
 public class LEDSignalCfg {
 
-    public static final int LED_PWM_PORT = 9;
+    public static final int LED_PWM_PORT = 0;
     public static final int LED_LENGTH = 60;
-    public static final Spark SIGNAL_LED = new Spark (LED_PWM_PORT);
+    public static final Spark SIGNAL_LED = new Spark(LED_PWM_PORT);
 
 
 }
